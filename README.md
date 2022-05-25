602bb525b8ac58a157a5b9e6f5e5fc73b38b36eb
TuXun1999 <617612390@qq.com>
2022-05-25 17:36:31 -0400

    A quick start

---
+++ README.md
@@ -0,0 +1,2 @@
+# Overview
+This repo contains the codes to evaluate ORB-SLAM3 and DSM. The evaluated datasets are KITTI (00-10, stereo) and EuRoC (MH01-05, stereo and monocular). The evaluation tool is based on evo
---
+++ slam/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/DependInfo.cmake
@@ -0,0 +1,64 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xun/ORB_SLAM3/src/Atlas.cc" "/home/xun/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/src/Atlas.cc.o"
+  "/home/xun/ORB_SLAM3/src/CameraModels/KannalaBrandt8.cpp" "/home/xun/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/src/CameraModels/KannalaBrandt8.cpp.o"
+  "/home/xun/ORB_SLAM3/src/CameraModels/Pinhole.cpp" "/home/xun/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/src/CameraModels/Pinhole.cpp.o"
+  "/home/xun/ORB_SLAM3/src/Config.cc" "/home/xun/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/src/Config.cc.o"
+  "/home/xun/ORB_SLAM3/src/Converter.cc" "/home/xun/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/src/Converter.cc.o"
+  "/home/xun/ORB_SLAM3/src/Frame.cc" "/home/xun/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/src/Frame.cc.o"
+  "/home/xun/ORB_SLAM3/src/FrameDrawer.cc" "/home/xun/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/src/FrameDrawer.cc.o"
+  "/home/xun/ORB_SLAM3/src/G2oTypes.cc" "/home/xun/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/src/G2oTypes.cc.o"
+  "/home/xun/ORB_SLAM3/src/GeometricTools.cc" "/home/xun/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/src/GeometricTools.cc.o"
+  "/home/xun/ORB_SLAM3/src/ImuTypes.cc" "/home/xun/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/src/ImuTypes.cc.o"
+  "/home/xun/ORB_SLAM3/src/KeyFrame.cc" "/home/xun/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/src/KeyFrame.cc.o"
+  "/home/xun/ORB_SLAM3/src/KeyFrameDatabase.cc" "/home/xun/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/src/KeyFrameDatabase.cc.o"
+  "/home/xun/ORB_SLAM3/src/LocalMapping.cc" "/home/xun/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/src/LocalMapping.cc.o"
+  "/home/xun/ORB_SLAM3/src/LoopClosing.cc" "/home/xun/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/src/LoopClosing.cc.o"
+  "/home/xun/ORB_SLAM3/src/MLPnPsolver.cpp" "/home/xun/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/src/MLPnPsolver.cpp.o"
+  "/home/xun/ORB_SLAM3/src/Map.cc" "/home/xun/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/src/Map.cc.o"
+  "/home/xun/ORB_SLAM3/src/MapDrawer.cc" "/home/xun/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/src/MapDrawer.cc.o"
+  "/home/xun/ORB_SLAM3/src/MapPoint.cc" "/home/xun/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/src/MapPoint.cc.o"
+  "/home/xun/ORB_SLAM3/src/ORBextractor.cc" "/home/xun/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/src/ORBextractor.cc.o"
+  "/home/xun/ORB_SLAM3/src/ORBmatcher.cc" "/home/xun/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/src/ORBmatcher.cc.o"
+  "/home/xun/ORB_SLAM3/src/OptimizableTypes.cpp" "/home/xun/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/src/OptimizableTypes.cpp.o"
+  "/home/xun/ORB_SLAM3/src/Optimizer.cc" "/home/xun/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/src/Optimizer.cc.o"
+  "/home/xun/ORB_SLAM3/src/Settings.cc" "/home/xun/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/src/Settings.cc.o"
+  "/home/xun/ORB_SLAM3/src/Sim3Solver.cc" "/home/xun/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/src/Sim3Solver.cc.o"
+  "/home/xun/ORB_SLAM3/src/System.cc" "/home/xun/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/src/System.cc.o"
+  "/home/xun/ORB_SLAM3/src/Tracking.cc" "/home/xun/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/src/Tracking.cc.o"
+  "/home/xun/ORB_SLAM3/src/TwoViewReconstruction.cc" "/home/xun/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/src/TwoViewReconstruction.cc.o"
+  "/home/xun/ORB_SLAM3/src/Viewer.cc" "/home/xun/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/src/Viewer.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  "HAVE_EIGEN"
+  "HAVE_GLEW"
+  "HAVE_PYTHON"
+  "ORB_SLAM3_EXPORTS"
+  "PANGO_DEFAULT_WIN_URI=\"wayland\""
+  "_LINUX_"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../include"
+  "../include/CameraModels"
+  "../Thirdparty/Sophus"
+  "/usr/local/include/eigen3"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
